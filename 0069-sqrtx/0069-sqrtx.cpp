393f14b7b44e07d855e861cca97f6a9a287a61ce
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-04-10 22:48:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.72%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l=0;
+        long long r=x;
+        long long ans=0;
+        while(l<=r){
+            long long mid = l + (r - l) / 2;
+            if(mid*mid==x)return mid;
+            if(mid*mid<=x){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
