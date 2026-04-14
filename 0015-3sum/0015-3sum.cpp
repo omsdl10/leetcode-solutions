f64eeaaac607f0a8fc921cd9f49db3d2e22b0a7a
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-04-14 19:18:06 +0530

    Time: 50 ms (48.27%), Space: 29 MB (72.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,7 +6,7 @@ class Solution{
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n-2;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
+            if(i>0 && nums[i]==nums[i-1])continue;
             int l=i+1,r=n-1;
             while(l<r){
             int sum=nums[i]+nums[l]+nums[r];
