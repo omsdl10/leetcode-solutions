b050b40598df35b7796d8dae42b34b640631bda0
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-06-08 22:29:47 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.34%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,18 +2,16 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans=[-1]*len(nums2)
         stack=[]
-        res=[]
+        mp={}
         for i in range(len(nums2)-1,-1,-1):
             while stack and stack[-1]<nums2[i]:
                 stack.pop()
             if stack:
                 ans[i]=stack[-1]
             stack.append(nums2[i])
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]==nums2[j]:
-                    res.append(ans[j])
-        return res
+            mp[nums2[i]]=ans[i]
+        
+        return [mp[x] for x in nums1]
 
 
         
