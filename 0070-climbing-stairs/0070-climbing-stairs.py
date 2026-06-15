9f6414943c98b6d12170bb954736940916c1c311
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-06-15 19:02:00 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,8 @@
-def helper(arr, n):
-    if arr[n] != -1:
-        return arr[n]
-
-    arr[n] = helper(arr, n-1) + helper(arr, n-2)
-    return arr[n]
-
-
 class Solution:
     def climbStairs(self, n: int) -> int:
         arr = [-1] * (n + 1)
-
         arr[0] = 1
         arr[1] = 1
-
-        return helper(arr, n)
+        for i in range(2,n+1):
+            arr[i]=arr[i-1]+arr[i-2]
+        return arr[n]
