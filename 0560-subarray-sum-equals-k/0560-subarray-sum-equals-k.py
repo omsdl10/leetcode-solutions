65a6964eaecb8dc8ca8ae29b40387fce256e0998
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-08-11 14:16:19 +0530

    Time: 24 ms (95.63%), Space: 21.9 MB (56.56%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,7 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dic={0:1}
         count=0
+        dic={0:1}
         summ=0
         for i in range(len(nums)):
             summ+=nums[i]
@@ -12,7 +12,3 @@ class Solution:
             else:
                 dic[summ]=1
         return count
-            
-                
-
-
