3bb0d6c1deda9225f055a9cbdfe7d986484332e6
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-08-16 15:26:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.77%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,13 @@
-def helper(i,arr,result,n,lis):
-    if i>=n:
-        result.append(lis[:])
-        return
-    
-    helper(i+1,arr,result,n,lis)
-    lis.append(arr[i])
-    helper(i+1,arr,result,n,lis)
-    lis.pop()
-
-
+def helper(arr,n,ls,array):
+    if n==len(arr):
+        array.append(ls.copy())
+        return 
+    ls.append(arr[n])
+    helper(arr,n+1,ls,array)
+    ls.pop()
+    helper(arr,n+1,ls,array)
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result=[]
-        n=len(nums)
-        helper(0,nums,result,n,[])
-        return result
+        array=[]
+        helper(nums,0,[],array)
+        return array
