3aa548524106f0fc19ac043a9c40184616a7badc
OM SINGH BISEN <104277734+omsdl10@users.noreply.github.com>
2026-08-16 15:19:51 +0530

    Time: 2 ms (65.04%), Space: 21.1 MB (43.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count=nums[0]
-        for i in range(1,len(nums)):
-            count^=nums[i]
+        count=0
+        for i in nums:
+            count^=i
         return count
